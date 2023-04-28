50c79629228a70a58af7d7382c4337c330a2a3ac
ludivineConstanti <ludivine.constanti@outlook.fr>
2023-04-28 22:19:51 +0200

    now works with wagmi and rainbow kit

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,39 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { polygonMumbai, polygon } from "wagmi/chains";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
 
 export default function MyApp({ Component, pageProps }) {
+  const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
+  const projectId = process.env.CLOUD_WALLET_PROJECT_ID;
+
+  const { chains, provider } = configureChains(
+    [polygonMumbai, polygon],
+    [infuraProvider({ apiKey: infuraId }), publicProvider()]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "web3rsvp",
+    projectId,
+    chains,
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  });
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
