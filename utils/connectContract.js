eb1dbc61c8dd0b45930f74a098d4f3debb3d7157
ludivineConstanti <ludivine.constanti@outlook.fr>
2023-04-29 14:41:20 +0200

    submitting an event now works

--- utils/connectContract.js
+++ utils/connectContract.js
@@ -3,8 +3,7 @@ import { ethers } from "ethers";
 
 function connectContract() {
   //Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
-  const contractAddress =
-    "0xf24919ee97943c8e076c10bf69a1f8d7988fb688dde8d10d02e89fd1adbb7902";
+  const contractAddress = "0xdcd45fdb917366181acd1d9b1bebcb7bf14f8f5b";
   const contractABI = abiJSON.abi;
   let rsvpContract;
   try {
@@ -15,11 +14,12 @@ function connectContract() {
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
       rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+      console.log(rsvpContract);
     } else {
       console.log("Ethereum object doesn't exist!");
     }
   } catch (error) {
-    console.log("ERROR:", error);
+    console.log("ERROR connectContract:", error);
   }
   return rsvpContract;
 }
